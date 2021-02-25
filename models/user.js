c7fcfc9fae239a85d4f03abca7a7e43c00c2a24b
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-25 14:08:20 -0600

    added columns in user model

--- models/user.js
+++ models/user.js
@@ -3,6 +3,14 @@ const bcrypt = require("bcryptjs");
 // Creating our User model
 module.exports = function(sequelize, DataTypes) {
   const User = sequelize.define("User", {
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
