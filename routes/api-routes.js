db983d1a2db9a68e2d4be7346ae93c2bde6e5aa0
Johanna L <johannarleal@gmail.com>
2021-02-25 19:47:13 -0500

    Fixed Lint errors

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,46 +52,44 @@ module.exports = function(app) {
   });
 
   // Get route for retrieving all lists
-  app.get('/api/lists', (req, res) => {
+  app.get("/api/lists", (req, res) => {
     // Add sequelize code to find all lists, and return them to the user with res.json
-    db.List.findAll({}).then((dbList) => res.json(dbList));
+    db.List.findAll({}).then(dbList => res.json(dbList));
   });
 
   // Get route for retrieving a single list
-  app.get('/api/lists/:id', (req, res) => {
+  app.get("/api/lists/:id", (req, res) => {
     // Add sequelize code to find a single list where the id is equal to req.params.id,
     // return the result to the user with res.json
-    db.dbList.findOne({
-      where: {
-        id: req.params.id
-      }
-    }).then((result) => {res.json(result)})
+    db.dbList
+      .findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(result => res.json(result));
   });
 
   // POST route for saving a new list
-  app.post('/api/lists', (req, res) => {
+  app.post("/api/lists", (req, res) => {
     // Add sequelize code for creating a post using req.body,
     // then return the result using res.json
-    db.List.create(
-      req.body
-    ).then((dbList) => res.json(dbList));
+    db.List.create(req.body).then(dbList => res.json(dbList));
   });
 
   // Update route to update an existing list
-  app.put('/api/lists', (req, res) => {
+  app.put("/api/lists", (req, res) => {
     // Add code here to update a list using the values in req.body, where the id is equal to
     // req.body.id and return the result to the user using res.json
     db.List.update(
       {
-        title: req.body.title,
+        title: req.body.title
       },
       {
         where: {
-          id: req.body.id,
-        },
+          id: req.body.id
+        }
       }
-    ).then((dbList) => res.json(dbList));
+    ).then(dbList => res.json(dbList));
   });
-
-
 };
