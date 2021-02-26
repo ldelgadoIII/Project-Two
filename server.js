9b06939f3c4e92340011c96d42c9057679243e20
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-26 10:52:27 -0600

    added feature that displays list titles to index.handlebars

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 
 const express = require("express");
 const session = require("express-session");
-// Requiring passport as we've configured it
 const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
@@ -19,20 +18,17 @@ app.set("view engine", "handlebars");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.use(express.static("public"));
-
 // Requiring our routes
-require("./routes/html-routes.js")(app);
+app.use(require("./routes/html-routes"));
 require("./routes/api-routes.js")(app);
 
-// We need to use sessions to keep track of our user's login status
+// Use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Syncing our database and logging a message to the user upon success
 db.sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => {
     console.log(
