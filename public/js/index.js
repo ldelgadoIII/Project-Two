1ea3529f402dc66d240cb8c24ec9369eb379d3e1
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-02-27 20:29:44 -0600

    uncommented list fetch request

--- public/js/index.js
+++ public/js/index.js
@@ -1,32 +1,32 @@
 // CREATE LIST
-// const createListBtn = document.getElementById("create-list");
+const createListBtn = document.getElementById("create-list");
 
-// createListBtn.addEventListener("submit", event => {
-//   event.preventDefault();
+createListBtn.addEventListener("submit", event => {
+  event.preventDefault();
 
-//   const newList = {
-//     title: document.getElementById("list-item").value.trim()
-//   };
+  const newList = {
+    title: document.getElementById("list-item").value.trim()
+  };
 
-//   console.log(newList);
+  console.log(newList);
 
-//   fetch("/api/lists", {
-//     method: "POST",
-//     headers: {
-//       Accept: "application/json",
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(newList)
-//   }).then(() => {
-//     // Empty the form
-//     document.getElementById("list-item").value = "";
+  fetch("/api/lists", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newList)
+  }).then(() => {
+    // Empty the form
+    document.getElementById("list-item").value = "";
 
-//     console.log("New task created");
+    console.log("New task created");
 
-//     // Reloads the page to see added task
-//     location.reload();
-//   });
-// });
+    // Reloads the page to see added task
+    location.reload();
+  });
+});
 
 // CREATE TASK
 const createTaskBtn = document.getElementById("create-task");
@@ -58,7 +58,7 @@ createTaskBtn.addEventListener("submit", event => {
     console.log("New task created");
 
     // Reloads the page to see added task
-    location.reload();
+    // location.reload();
   });
 });
 
