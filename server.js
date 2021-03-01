fc1b2c743e698fee4be9764480c6d00bcf00f344
Loreto Delgado III <ldelgadothethird@gmail.com>
2021-03-01 09:48:24 -0600

    removed previous changes to server. connected to server using http

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
 const express = require("express");
+const session = require("express-session");
 const path = require("path");
+const passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -10,6 +12,7 @@ const db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 const app = express();
 const exphbs = require("express-handlebars");
+
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
@@ -21,26 +24,21 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.get("/chat", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// Requiring our routes
+app.use(require("./routes/html-routes"));
+require("./routes/api-routes.js")(app);
 
-app.get("/dash", (req, res) => {
-  res.sendFile(__dirname + "/dashboard.html");
-});
+// Use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 io.on("connection", socket => {
   console.log("a user connected");
   socket.on("disconnect", () => {
     console.log("user disconnected");
-  });
-
-  socket.on("chat message", msg => {
-    io.emit("chat message", msg);
-  });
-
-  socket.on("count", count => {
-    io.emit("count", count);
   });
 });
 
@@ -53,16 +51,3 @@ db.sequelize.sync({ force: false }).then(() => {
     );
   });
 });
-
-// const session = require("express-session");
-// const passport = require("./config/passport");
-// // Requiring our routes
-// app.use(require("./routes/html-routes"));
-// require("./routes/api-routes.js")(app);
-
-// // Use sessions to keep track of our user's login status
-// app.use(
-//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-// );
-// app.use(passport.initialize());
-// app.use(passport.session());
